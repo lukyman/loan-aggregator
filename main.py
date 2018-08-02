a59841e467a620b8b742b112ae1baa5ded1d8bf2
mzawadi <tech@mzawadi.com>
2018-08-02 06:51:17 +0300

    separated method to read from csv (read_loans_from_csv) and aggregate(aggregate_loan)

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import loan_aggregator
 
 class Main():
     """ Entry to loan aggregator program execution """
+    
     def __init__(self):
+        self.data_list = []
         self.read_loans_from_csv()
-    pass
+        self.aggregate_loan()
 
     def write_to_csv(self, data, filename, delimiter):
         csv.register_dialect('simple_dialect', 
@@ -20,34 +22,29 @@ class Main():
             writer.writerow(data)
     
     def read_loans_from_csv(self):
-
         with open('Loans.csv') as loansfile:
             loandata = csv.reader(loansfile)
-            data_list = util.csv_to_list_dict(loandata)
+            self.data_list = util.csv_to_list_dict(loandata)
+           
+    def aggregate_loan(self):
+         #Aggrgate by month
+        grouped_by_Date =loan_aggregator.groupby_column(
+            self.data_list,'Date', util.getMonthName)
+        aggregate_month = loan_aggregator.aggregate_by_column(
+            grouped_by_Date,['Month', 'Total Amount','Counts'])
+        #Aggregate by product
+        grouped_by_product = loan_aggregator.groupby_column(
+            self.data_list,'Product', util.removeExtraDoubleQuotation)
+        aggregate_product = loan_aggregator.aggregate_by_column(
+            grouped_by_product,['Product', 'Total Amount','Counts'])
+        #Aggregate by Network
+        grouped_by_network = loan_aggregator.groupby_column(
+            self.data_list,'Network', util.removeExtraDoubleQuotation)
+        aggregate_network = loan_aggregator.aggregate_by_column(
+            grouped_by_network,['Network', 'Total Amount','Counts'])
+        output_data = (aggregate_network, aggregate_product, aggregate_month)
+        #call the method to write to csv file
+        self.write_to_csv(output_data,'Output.csv', ',')
 
-            #Aggrgate by month
-            grouped_by_Date =loan_aggregator.groupby_column(
-                data_list,'Date', util.getMonthName)
-
-            aggregate_month = loan_aggregator.aggregate_by_column(
-                grouped_by_Date,['Month', 'Total Amount','Counts'])
-
-            #Aggregate by product
-            grouped_by_product = loan_aggregator.groupby_column(
-                data_list,'Product', util.removeExtraDoubleQuotation)
-
-            aggregate_product = loan_aggregator.aggregate_by_column(
-                grouped_by_product,['Product', 'Total Amount','Counts'])
-
-            #Aggregate by Network
-            grouped_by_network = loan_aggregator.groupby_column(
-                data_list,'Network', util.removeExtraDoubleQuotation)
-
-            aggregate_network = loan_aggregator.aggregate_by_column(
-                grouped_by_network,['Network', 'Total Amount','Counts'])
-
-            output_data = (aggregate_network, aggregate_product, aggregate_month)
-            #call the method to write to csv file
-            self.write_to_csv(output_data,'Output.csv', ',')
 if __name__=="__main__":
     Main()
